4d5cfe761d4b12d74f67e51fe74ad5f0764fc7f5
paalch <pciverser@stud.ntnu.no>
2017-02-21 14:41:38 +0100

    Added TAin to models

--- bota/course/models.py
+++ bota/course/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Course(models.Model):
-    CourseID = models.CharField(max_length=7)
+    CourseID = models.CharField(max_length=10)
+    Name = models.CharField(max_length=20, default='TBA')
+    Term = models.CharField(max_length=10, default='TBA')
     Description = models.CharField(max_length=45)
 
     def __str__(self):
         return self.CourseID
 
 class Takes(models.Model):
+    CourseID = models.ForeignKey(Course)
+    UserID= models.ForeignKey(User)
+
+class TAin(models.Model):
     CourseID = models.ForeignKey(Course)
     UserID= models.ForeignKey(User)
