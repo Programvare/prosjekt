26cfe9f22301042385e32ca75c736899a0c745bf
Unknown <pernillej11@gmail.com>
2017-02-23 17:09:19 +0100

    edited register on admin page

--- bota/bota/course/admin.py
+++ bota/bota/course/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Course, TATime
+from .models import Course, TATime, Takes, TAin
 
 admin.site.register(Course)
 admin.site.register(TATime)
+admin.site.register(Takes)
+admin.site.register(TAin)
