de19faf94de78d5fc52359951f9c6fed581ddfab
synnovsh <synnovsh@stud.ntnu.no>
2017-03-08 15:35:58 +0100

    cleaned up, refined, added comments

--- bota/bota/course/urls.py
+++ bota/bota/course/urls.py
@@ -24,11 +24,7 @@ urlpatterns = [
     url(r'((?P<courseid>[A-Z]{3}\d+)/$)', views.course, name='course'),
     url(r'((?P<courseid>[A-Z]{3}\d+)/inQueue)', views.addMeToList),
     url(r'((?P<courseid>[A-Z]{3}\d+)/rmQueue)', views.removeFromCourse),
-    #TODO: prob cant remove this, can we?
-    #NOTE: name allows reverse lookup in jinja, i.e. {% url 'name'% }
-    #url(r'((?P<courseid>[A-Z]{3}\d+)/course_position)', views.course_position, name='course_position'),
     url(r'^course_position/$', views.course_position, name='course_position'),
-
 ]
 
 """url(r'((?P<courseid>[A-Z]{3}\d+)/ta_time)', views.ta_time, name='ta_time'),"""
