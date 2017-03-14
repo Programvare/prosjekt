506bc6b52fcc6a8dad6126da5557b96eb5c9a88c
Paalch <pciverser@stud.ntnu.no>
2017-03-14 19:54:31 +0100

    added bootsrap fixed static folder

--- bota/bota/settings.py
+++ bota/bota/settings.py
@@ -142,8 +142,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+import os.path
 
+STATIC_ROOT = ''
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
 
 LOGOUT_REDIRECT_URL = '/'
