b25efc59c446b47fafad64eaf4444aed4a628aed
synnovsh <synnovsh@stud.ntnu.no>
2017-03-30 14:36:51 +0200

    Add statement to allow switch between heroku db and local db

--- bota/bota/settings.py
+++ bota/bota/settings.py
@@ -95,13 +95,20 @@ DATABASES = {
     }
 }
 """
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+import dj_database_url
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 
