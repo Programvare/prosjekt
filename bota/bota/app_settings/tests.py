5ef9f82d3352630ed5d691416d405cc60b5a699b
Paalch <pciverser@stud.ntnu.no>
2017-04-01 20:48:25 +0200

    added test to urls

--- bota/bota/app_settings/tests.py
+++ bota/bota/app_settings/tests.py
@@ -1,12 +1,15 @@
 from django.test import Client
 from django.test import TestCase
 from bota.course.models import Course, Assignment
+from django.contrib.auth.models import User
 
 
 class RequestPageTests(TestCase):
     def setUp(self):
         course = Course.objects.create(course_id="TDT4140", name="Programvare utvikling", nickname="pu",description="testing")
         Assignment.objects.create(course=course, description="testing", name="Øving 1", delivery_deadline="1990-05-04 22:15", demo_deadline="1990-05-04 22:15" )
+        User.objects.create_user(username='testuser', password='4epape?Huf+V')
+        User.objects.create_user(username='testadmin', password='4epape?Huf+V', is_staff='True')
 
     def test_get_pages_as_anonymous_access(self):
         client = Client()
@@ -67,6 +70,98 @@ class RequestPageTests(TestCase):
         self.assertEqual(302, request.status_code)
         self.assertEqual('/login/', request.url[:7])
 
+    def test_getPages_admin(self):
+        client = Client()
+        client.login(username='testadmin', password='4epape?Huf+V')
+
+        request = client.get('/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/login/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/course/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/courses/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/courses/add_course/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/courses/TDT4140/edit')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/TDT4140/add_ta')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/TDT4140/add_ta/testuser/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/add_as/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/TDT4140/edit_as/1/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/TDT4140/rm_ta/testuser/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/rm_as/1/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/courses/TDT4140/rm_course/')
+        self.assertEqual(302, request.status_code)
+
+
+    def test_getPages_user(self):
+        client = Client()
+        client.login(username='testuser', password='4epape?Huf+V')
+
+        request = client.get('/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/login/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/course/')
+        self.assertEqual(200, request.status_code)
+
+        request = client.get('/settings/courses/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/courses/add_course/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/courses/TDT4140/edit')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/add_ta')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/add_ta/testuser/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/add_as/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/edit_as/1/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/rm_ta/testuser/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/TDT4140/rm_as/1/')
+        self.assertEqual(302, request.status_code)
+
+        request = client.get('/settings/courses/TDT4140/rm_course/')
+        self.assertEqual(302, request.status_code)
 
 
 
