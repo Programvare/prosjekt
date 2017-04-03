1c3b1c3689ac3c555fa2f03e087f6c5be2ce867b
synnovsh <synnovsh@stud.ntnu.no>
2017-04-03 19:37:53 +0200

    Add back login required to main course page

--- bota/bota/course/views.py
+++ bota/bota/course/views.py
@@ -15,6 +15,7 @@ def course_main_page(request):
     return render(request, 'main_course_page.html', context)
 
 
+@login_required(login_url='/login/')
 def course(request, course_id):
 
     ta_times = get_week_times(course_id)
@@ -39,40 +40,6 @@ def course(request, course_id):
     if queue.user_in_queue(request.user, course_id):
         return render(request, 'course_in_queue.html', context)
     return render(request, 'course.html', context)
-
-
-def course_position(request):
-    #The problem with having a separate view for a _div_
-    #is that we can't have a fancy context-based url in urls.py
-    #request.META gives the current url path. index [-2] should return the current courseid
-    course_id = request.META['HTTP_REFERER'].split('/')[-2]
-    position = queue.get_position(request.user, course_id)
-
-    context = {
-        'course_model': Course.objects.get(course_id=course_id),
-        'position': position,
-        'course_id': course_id,
-    }
-    return render(request, 'course_position_div.html', context)
-
-
-@login_required(login_url='/login/')
-def course_ta_next(request):
-    #The problem with having a separate view for a _div_
-    #is that we can't have a fancy context-based url in urls.py
-    #request.META gives the current url path. index [-2] should return the current courseid
-    course_id = request.META['HTTP_REFERER'].split('/')[-2]
-    next_queue = queue.get_next(course_id)
-    queue_length = queue.get_length(course_id)
-
-    context = {
-        'next': next_queue,
-        'queue_length': queue_length,
-        'course_model': Course.objects.get(course_id=course_id),
-        'course_id': course_id,
-    }
-    return render(request, 'course_ta_next_div.html', context)
-
 
 @login_required(login_url='/login/')
 def course_ta(request, course_id):
@@ -105,22 +72,47 @@ def rm_from_course(request, course_id):
 
 
 """
-Removed and placed in div
-@login_required(login_url='/login/')
-def taTimes(request, courseid):
-    all_tatimes = get_all_times(courseid)
-
-    context = {
-        'ta_time_list': all_tatimes,
-        'ccourse': courseid
-    }
-
-    template = loader.get_template('ta_time.html')
-    return HttpResponse(template.render(context, request))
+The two following views are made for the purpose of autorefreshing divs with js.
+By creating views, they have separate URLs, with their own POST and GET requests.
+This is done so they can be refreshed separately,
 """
 
-# Help functions #
 
+def course_position(request):
+    #The problem with having a separate view for a _div_
+    #is that we can't have a fancy context-based url in urls.py
+    #request.META gives the current url path. index [-2] should return the current courseid
+    course_id = request.META['HTTP_REFERER'].split('/')[-2]
+    position = queue.get_position(request.user, course_id)
+
+    context = {
+        'course_model': Course.objects.get(course_id=course_id),
+        'position': position,
+        'course_id': course_id,
+    }
+    return render(request, 'course_position_div.html', context)
+
+
+def course_ta_next(request):
+    #The problem with having a separate view for a _div_
+    #is that we can't have a fancy context-based url in urls.py
+    #request.META gives the current url path. index [-2] should return the current courseid
+    course_id = request.META['HTTP_REFERER'].split('/')[-2]
+    next_queue = queue.get_next(course_id)
+    queue_length = queue.get_length(course_id)
+
+    context = {
+        'next': next_queue,
+        'queue_length': queue_length,
+        'course_model': Course.objects.get(course_id=course_id),
+        'course_id': course_id,
+    }
+    return render(request, 'course_ta_next_div.html', context)
+
+
+"""
+Helper functions, not views.
+"""
 
 def get_all_times(course_id):
     # Get list of all ta times for course
