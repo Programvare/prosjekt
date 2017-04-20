a57af3715186ea20ab617592953a927f1369153e
Unknown <pernillej11@gmail.com>
2017-04-20 12:34:13 +0200

    fixed running on localshost

--- bota/bota/settings.py
+++ bota/bota/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ala_fiz=7j(xua@t2vb&uqj3zxg0ki2+7i8uc1437_^n3nor5l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['botaapp.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['botaapp.herokuapp.com', 'localhost', u'127.0.0.1']
 
 
 # Application definition
@@ -98,8 +98,8 @@ DATABASES = {
 in_heroku = False
 if 'DATABASE_URL' in os.environ:
     in_heroku = True
+    import dj_database_url
 
-import dj_database_url
 if in_heroku:
     DATABASES = {'default': dj_database_url.config()}
 else:
